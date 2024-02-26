133131db0b667e0871526e2b207c899efd564eea
Bruno Coutinho Remeikis <53705186+bruno-remeikis@users.noreply.github.com>
2024-02-26 09:21:07 -0300

    Adds MySQL connection

--- index.js
+++ index.js
@@ -1,11 +1,54 @@
 const express = require('express')
+const connection = require('./connection')
 
 const app = express()
 const port = 3333
 
-app.get('/', (req, res) =>
-{
-    return res.json("API rodando na AWS!")
+app.get('/', (req, res) => res.json("API rodando na AWS!"))
+
+app.get('/users', async (req, res) => {
+    try {
+        const [results] = await connection.execute('SELECT * FROM USER')
+        res.json(results);
+    }
+    catch(err) {
+        console.error(err)
+        res.status(400).json(`ERRO não tratado: ${err}`)
+    }
+})
+
+app.get('/create-user', async (req, res) => {
+    try {
+        if(!req.query.name || !req.query.email)
+            res.status(400).json("ERRO: Para criar um novo usuário, informe 'name' e 'email' por query params.")
+
+        const values = [ req.query.name, req.query.email ]
+
+        await connection.query("INSERT INTO USER (NAME, EMAIL) VALUES (?, ?)", values);
+
+        res.status(201).json(`Usuário '${req.query.name}' criado com sucesso!`);
+    }
+    catch(err) {
+        console.error(err)
+        res.status(400).json(`ERRO não tratado: ${err}`)
+    }
+})
+
+app.get('/delete-user', async (req, res) => {
+    try {
+        if(!req.query.id)
+            res.status(400).json("ERRO: Para deletar um usuário, informe 'id' por query params.")
+
+        const values = [ req.query.id ]
+
+        await connection.query("DELETE FROM USER WHERE ID = ?", values);
+
+        res.status(201).json(`Usuário com ID ${req.query.id} deletado com sucesso!`);
+    }
+    catch(err) {
+        console.error(err)
+        res.status(400).json(`ERRO não tratado: ${err}`)
+    }
 })
 
 // app.post('/teste', (res, res) =>
@@ -15,7 +58,4 @@ app.get('/', (req, res) =>
 //     return res.json({ text });
 // })
 
-app.listen(port, () =>
-{
-    console.log(`Listening on port ${port}...`);
-});
+app.listen(port, () => console.log(`Listening on port ${port}...`))
